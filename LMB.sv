78daa5a72054c15439995ee8380a967d53b906b1
Anurag Singh <anuragsi003@gmail.com>
2023-08-11 17:43:26 +0530

    Update LMB.sv

--- LMB.sv
+++ LMB.sv
@@ -1,2 +1,13 @@
 ///Calculating Leftmost Set bit//////////////////
 Approach-1: Right Shift the Number till its greater than 0 (>0) . 
+module LMB (
+            input logic clk;
+  input logic [NUM_PORTS-1:0] in,
+  output logic pos );
+
+
+  always_comb
+    begin   
+      while(n>0)
+          y=x>>1;
+      
